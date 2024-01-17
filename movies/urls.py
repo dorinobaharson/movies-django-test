fcc1f28de939adb878e0b003167acadc7af39aa2
dorinobaharson <dorinobaharson1@gmail.com>
2024-01-17 11:23:34 +0700

    Final

--- movies/urls.py
+++ movies/urls.py
@@ -21,5 +21,8 @@ from movies import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('movies/', views.movies),
-    path('', views.home)
+    path('', views.home),
+    path('movies/<int:id>', views.detail),
+    path('movies/add', views.add),
+    path('movies/delete/<int:id>', views.delete)
 ]
