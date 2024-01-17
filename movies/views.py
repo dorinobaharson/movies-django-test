5f61c81d48f5634f2cc55d4b51f6e3e6c7f4c0de
dorinobaharson <dorinobaharson1@gmail.com>
2024-01-17 10:20:38 +0700

    sebelum data besar

--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,29 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+data = {
+    'movies': 
+        [
+            {
+                'id' : 1,
+                'title' : "The Shawshank Redemption",
+                'year' : 1994,
+            },
+            {
+                'id' : 2,
+                'title' : "The Godfather",
+                'year' : 1972,
+            },
+            {
+                'id' : 3,
+                'title' : "The Dark Knight",
+                'year' : 2008,
+            }
+        ]
+    }
+
 def movies(request):
-    return render(request, "")
+    return render(request, 'movies/movies.html', data)
 
 def home(request):
     return HttpResponse("Home")
