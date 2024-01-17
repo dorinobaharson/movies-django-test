b65a7e8080af471a915fb42a4cb5199881d0163f
dorinobaharson <dorinobaharson1@gmail.com>
2024-01-17 09:50:48 +0700

    test commit

--- movies/urls.py
+++ movies/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from movies import views
 
 urlpatterns = [
-    path('admin1/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('movies/', views.movies),
+    path('', views.home)
 ]
