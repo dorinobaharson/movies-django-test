7dc820d118215d62c5ee6e1b68b6eb130e437802
dorinobaharson <dorinobaharson1@gmail.com>
2024-01-17 09:52:16 +0700

    add baru

--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def movies(request):
-    return HttpResponse("Hello World")
+    return render(request, "")
 
 def home(request):
     return HttpResponse("Home")
